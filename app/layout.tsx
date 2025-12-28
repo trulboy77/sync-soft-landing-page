e1af859065fed8110c33a710fe7aa765ae951ea9
trulboy77 <trulboy77@gmail.com>
2025-12-28 05:34:04 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'OUTSOURCING',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'SyncSoft Uz - Outsorcing',
+  description: 'SyncSoft',
+  generator: 'SyncSoft UZ',
   icons: {
     icon: [
       {
